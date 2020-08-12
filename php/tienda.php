2c08c6fdf13bb409f47407f17faebb016cdd0b6d
Jemiroge <48736572+Jemiroge@users.noreply.github.com>
2020-08-12 01:07:39 -0500

    subiendo de nuevo

---
+++ php/tienda.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ciudad verde</title>
+	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+	<body class="container-fluid borverde">
+	
+
+		
+	<header class="jumbotron container-fluid" style="height:400px;">
+        
+
+		   
+
+		  
+
+		  
+
+	</header>	
+
+<aside id="i" class="container-fluid badge badge-success col-12 h-300">
+
+
+	
+	
+	</aside>
+
+    	 
+    <marquee style="background:#28a745;color:white;font-size: 50px;border-radius:10%;"><h1>NUESTRO LOGO Y DIRECCION PARA QUE TENGAS CLARO LO QUE SOMOS </h1></marquee>
+	<article  id="u"	class="container-fluid borverde">
+
+			
+	</article>
+
+	
+
+	<footer id="m" class="container-fluid borderes  h-200 badge badge-success ">
+		
+		
+	</footer>
+	
+	
+</body>
+</html>
