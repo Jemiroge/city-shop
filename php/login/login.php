4bc7796e881988b9b9ad35a09805a700276f13a9
Jemiroge <48736572+Jemiroge@users.noreply.github.com>
2020-08-13 23:28:10 -0500

    modificando login

--- php/login/login.php
+++ php/login/login.php
@@ -8,20 +8,32 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
 </head>
 
-<body background="../../img/plogueo/fondo1.jpg" class="fondo">
-	<br><br><br><br>
-	<h1 class="col-2 float-left"><span class="badge badge-success float-left display-1 ">Ciudad Verde Shop </span></h1> 
-	<br><br>
-	<div class="container-fluid col-3">
+<body background="../../img/plogueo/fondo2.jpg" class="jumbotron fondo container-fluid col-4">
+	<br><br><br><br>	
+	<div>
+		<h1 class="container-fluid badge badge-success float-left"><span class="letra">Ciudad Verde Shop </span></h1>
+
+	 
+	 <br><br><br><br>
+
+
 	    <form action="">
-	    <h1 class="blanco radio">Login <img class="radio" src="../../img/plogueo/hoja.jpg"></h1>	
-		<input  class="trans" type="text" placeholder="Usuario">
+	    <h1 class="badge badge-success display-1 container-fluid">Logueate a </h1>	
+	    <br><br>
+		<input class="trans" type="text" placeholder="Usuario">
 		<br><br>
-		<input  class="trans" type="text" placeholder="Contraseña" >
+		<input class="trans"  type="text" placeholder="Contraseña" >
 		<br><br>
-		<button type="button" class="btn btn-success"><a href="#i" class="blanco">Enviar</a></button>
+
+		<button type="button" class="trans btn btn-success "><a class="blanco" href="#i">Ingresar</a></button>		
+		<br>
+		<br>
+		<a href="../registrate/registrate.php" class="blanco">Olvide mi contraseña</a>
+		<a href="../registrate/registrate.php" class="blanco">¿Aun no te has registrado? ¡registrate!</a>
+
         
 
 	   </form>
