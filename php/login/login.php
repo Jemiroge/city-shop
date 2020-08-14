f850930052a4a0f63dccb1e110b3246eedc4b502
Jemiroge <48736572+Jemiroge@users.noreply.github.com>
2020-08-13 21:47:11 -0500

    modifidificando carpetas css

--- php/login/login.php
+++ php/login/login.php
@@ -3,20 +3,20 @@
 <head>
 	<meta charset="UTF-8">
 	<title>login</title>
-	<link rel="stylesheet" href="../css/login.css" type="text/css" >
+	<link rel="stylesheet" href="../../css/login/login.css" type="text/css" >
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 
-<body background="../img/login/imajpg.jpg" class="fondo">
+<body background="../../img/plogueo/fondo1.jpg" class="fondo">
 	<br><br><br><br>
 	<h1 class="col-2 float-left"><span class="badge badge-success float-left display-1 ">Ciudad Verde Shop </span></h1> 
 	<br><br>
 	<div class="container-fluid col-3">
 	    <form action="">
-	    <h1 class="blanco radio">Login <img class="radio" src="../img/login/hoja.jpg"></h1>	
+	    <h1 class="blanco radio">Login <img class="radio" src="../../img/plogueo/hoja.jpg"></h1>	
 		<input  class="trans" type="text" placeholder="Usuario">
 		<br><br>
 		<input  class="trans" type="text" placeholder="Contraseña" >
